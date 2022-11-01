b632493f234b9facb60acaf7347c9e294c877450
Marchabar <marchabar1@alum.us.es>
2022-11-01 21:15:01 +0100

    commit test

--- src/main/java/org/springframework/samples/petclinic/product/ProductController.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductController.java
@@ -1,5 +1,41 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class ProductController {
+
+
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+    @GetMapping("/product/create")
+    public ModelAndView addProduct(Product p){
+        ModelAndView result = new ModelAndView("createOrUpdateProductForm");
+        result.addObject("product", p);
+        return result;
+
+
+    }
+
+    @PostMapping("/product/create")
+    public ModelAndView saveAddedProduct(@Valid Product p, BindingResult br){
+        if(!br.hasErrors()){
+            productService.save(p);
+        }
+        return new ModelAndView("redirect:/welcome");
+
+    } 
     
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,13 +2,30 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+    
+    private ProductRepository productRepo;
+
+    @Autowired
+    public ProductService(ProductRepository productRepo){
+        this.productRepo = productRepo;
+    }
+    public List<ProductType> findAllProductTypes(){
+        return productRepo.findAllProductTypes();
+    }
     public List<Product> getAllProducts(){
-        return null;
+        return productRepo.findAll();
     }
 
+    public Product getProductByName(String name){
+        return productRepo.findByName(name);
+    }
     public List<Product> getProductsCheaperThan(double price) {
-        return null;
+        return productRepo.findProductsCheaperThan(price);
     }
 
     public ProductType getProductType(String typeName) {
